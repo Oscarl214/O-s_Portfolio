c8663ca4c9ecb78f8f636c10e25a59ae0fab7711
Oscar Leal <oscarleal234@gmail.com>
2023-02-22 12:19:41 -0600

    load fade, first javascript added

--- javascript.js
+++ javascript.js
@@ -1,1 +1,9 @@
-// HOPING TO ADD JS TO BE ABLE TO HAVE BUTTON ONLY APPEAR WHEN USER SCROLLS PAST HEADER SECTION
+// HOPING TO ADD JS TO BE ABLE TO HAVE BUTTON ONLY APPEAR WHEN USER SCROLLS PAST HEADER SECTIO
+
+function doStuff(callback) {
+  callback();
+}
+
+doStuff(function () {
+  document.body.className = "visible";
+});
